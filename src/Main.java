cfff1879459b996a951a9e9c0c549d3568b2ee4e
Matheu <matheu.castelo@hotmail.com>
2023-03-07 18:59:01 -0300

    Commit classes: Curso, Mentoria e Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import com.dio.desafio.dominio.Curso;
+import com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Curso 1
+        Curso c = new Curso();
+        c.setTitulo("Curso de Poo");
+        c.setCargaHoraria(10);
+        c.setDesc("CURSO DE POO");
+
+        System.out.println(c.toString());
+
+        //Curso 2
+        Curso p = new Curso();
+        p.setTitulo("Curso de Pentest");
+        p.setCargaHoraria(150);
+        p.setDesc("CURSO DE PENTEST WEB");
+
+        System.out.println(p.toString());
+
+        //Mentoria
+        Mentoria m = new Mentoria();
+        m.setData(LocalDate.now());
+        m.setTitulo("Mentoria Java POO");
+        m.setDesc("Mentoria");
+
+        System.out.println(m.toString());
+
     }
 }
---
+++ src/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,44 @@
+package com.dio.desafio.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String desc;
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", desc='" + desc + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
